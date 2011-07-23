a9a0e9a3150bce2114626e7eb2d533a30683687b
matthias.bock <matthias.bock@hu-berlin.de>
2011-07-23 21:17:08 +0000

    url.py

--- urls.py
+++ urls.py
@@ -1,5 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
+	(r'^mediacenter/Music/$',			"Django.mediacenter.music.TitleList"),
+	(r'^mediacenter/Music/AddOnlineMusic$',		"Django.mediacenter.music.AddOnlineMusic"),
+	(r'^mediacenter/Music/LocalMusic$',		"Django.mediacenter.music.LocalMusic"),
+	(r'^mediacenter/Music/AddLocalMusic$',		"Django.mediacenter.music.AddLocalMusic"),
+	(r'^mediacenter/Music/Performers$',		"Django.mediacenter.music.PerformerList"),
+	(r'^mediacenter/Music/Performer$',		"Django.mediacenter.music.Performer"),
+	(r'^mediacenter/Music/PerformerIcon$',		"Django.mediacenter.music.PerformerIcon"),
+	(r'^mediacenter/Movies/$',			"Django.mediacenter.movies.Listing"),
+	(r'^mediacenter/Movies/Icon$',			"Django.mediacenter.movies.Icon"),
+	(r'^mediacenter/Movies/Search$',		"Django.mediacenter.movies.Search"),
+	(r'^mediacenter/Movies/AddMovie$',		"Django.mediacenter.movies.AddMovie"),
+	(r'^mediacenter/Movies/AddStream$',		"Django.mediacenter.movies.AddStream"),
+	(r'^mediacenter/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/mediacenter/static', 'show_indexes': True}),
 )
 
