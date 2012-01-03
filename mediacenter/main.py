768939ca14fff453bec46cfbd8756539ea390bf8
matthias.bock <matthias.bock@hu-berlin.de>
2012-01-03 14:29:19 +0000

    Radio works

--- mediacenter/main.py
+++ mediacenter/main.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponseRedirect('Music/')
+	return render_to_response('MediaCenter.html')
 
--- urls.py
+++ urls.py
@@ -3,17 +3,18 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
 	(r'^MediaCenter/$',				"Django.mediacenter.main.index"),
 
+	(r'^MediaCenter/Playlist$',			"Django.mediacenter.player.Playlist"),
+	(r'^MediaCenter/Player$',			"Django.mediacenter.player.Player"),
+
 	(r'^MediaCenter/Music/$',			"Django.mediacenter.music.index"),
 	(r'^MediaCenter/Music/TitleList$',		"Django.mediacenter.music.TitleList"),
 	(r'^MediaCenter/Music/AddURL$',			"Django.mediacenter.music.AddURL"),
 	(r'^MediaCenter/Music/IconListing$',		"Django.mediacenter.music.IconListing"),
 	(r'^MediaCenter/Music/Details$',		"Django.mediacenter.music.Details"),
 	(r'^MediaCenter/Music/Icon$',			"Django.mediacenter.music.Icon"),
-	(r'^MediaCenter/Music/Playlist$',		"Django.mediacenter.player.Playlist"),
-	(r'^MediaCenter/Music/Player$',			"Django.mediacenter.player.Player"),
 
 	(r'^MediaCenter/Radio/$',			"Django.mediacenter.radio.Listing"),
-	(r'^MediaCenter/Radio/Logo$',			"Django.mediacenter.radio.Logo"),
+	(r'^MediaCenter/Radio/Logo$',			"Django.mediacenter.radio.Icon"),
 
 	(r'^MediaCenter/Movies/$',			"Django.mediacenter.movies.Listing"),
 	(r'^MediaCenter/Movies/Icon$',			"Django.mediacenter.movies.Icon"),
