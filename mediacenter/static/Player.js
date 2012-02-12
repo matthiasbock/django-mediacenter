a9a6357bca5ae571c329b9a23089d4c65ad6e3ed
matthias.bock <matthias.bock@hu-berlin.de>
2012-02-12 19:42:35 +0000

    menu.html

--- mediacenter/static/Player.js
+++ mediacenter/static/Player.js
@@ -17,16 +17,3 @@ function extractplayer(URL) {
 	return xmlHttp.responseText;
 	}
 
-function insertplayer(player) {
-	parent.Player.document.getElementById('Player').innerHTML = player;
-	}
-
-function play(URL) {
-	 insertplayer(	'<object id="MediaPlayer" height="220" width="220" type="application/x-oleobject" standby="Loading Player ...">'
-			+ '<embed name="MediaPlayer" type="application/x-mplayer2" src="'+URL+'" pluginspage="http://mplayerplug-in.sourceforge.net/" width="220" height="220" displaysize="0" showstatusbar="1" autosize="0" showpositioncontrols="0" showaudiocontrols="1" showcontrols="true" animationatstart="0" autostart="1" transparentatstart="1" />'
-			+ '</object>' );
-	}
-
-function stop() {
-	document.getElementById('Player').innerHTML = '';
-	}
